0e41e82c894a546054649d601530e38cfb0783ed
tantran1012 <57386469+tantran1012@users.noreply.github.com>
2022-06-21 00:39:26 +0700

    Update Customers, add plugin datatables
    Cập nhật customers, thêm plugin datatables, sửa vài cái không nhớ.

--- components/customers/customers.controller.js
+++ components/customers/customers.controller.js
@@ -1,23 +1,56 @@
-const khachHang = require('./customers.service');
+const khachHang = require("./customers.model");
+const Handlebars = require("handlebars");
 
 class CustomerController {
-    index (req, res, next) {
-        khachHang.find({}) 
-            .then(customers => {
-                customers = customers.map(customers => customers.toObject());
-                res.render('customers/customers', {
-                    title: "Khách hàng",
-                    customers: customers,
-                });
-            })
-            .catch(next)
-    };
-
-    search (req, res, next) {
-        res.render('customers/customers', {
-            title: "Tìm kiếm Khách hàng",
+  index(req, res, next) {
+    khachHang.find({})
+      .then((customers) => {
+        customers = customers.map((customers) => customers.toObject());
+        res.render("customers/customers", {
+          title: "Khách hàng",
+          customers: customers,
         });
-    }
+      })
+      .catch(next);
+  }
+
+  create(req, res, next) {
+    res.render("customers/customers_edit", {
+      title: "Thêm khách hàng",
+    });
+  }
+
+  save(req, res, next) {
+    const customer = new khachHang(req.body);
+    customer.save()
+      .then(() => res.redirect("/customers"))
+      .catch(next);
+  }
+
+  edit(req, res, next) {
+    khachHang
+      .findOne({ ID: req.params.id })
+      .then((customer) => {
+        customer = customer.toObject();
+        res.render("customers/customers_edit", {
+          title: "Chỉnh sửa khách hàng",
+          customer: customer,
+        });
+      })
+      .catch(next);
+  }
+
+  update(req, res, next) {
+    khachHang.updateOne({ ID: req.params.id }, req.body)
+      .then(() => res.redirect('/customers'))
+      .catch(next);
+  }
+
+  delete(req, res, next) {
+    khachHang.deleteOne({ ID: req.params.id })
+      .then(() => res.redirect('back'))
+      .catch(next);
+  }
 }
 
-module.exports = new CustomerController;
+module.exports = new CustomerController();
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const hbs = require("express-handlebars");
 const path = require("path");
+const methodOverride = require('method-override')
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 3000;
 
@@ -13,9 +14,20 @@ require("dotenv").config();
 
 const app = express();
 
+app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, "public")));
 // view engine setup
-app.engine("hbs", hbs({ extname: ".hbs" }));
+app.engine("hbs", hbs({ 
+	extname: ".hbs",
+	helpers: {
+		ifEquals: (a, b, options) => {
+			if (a === b) {
+				return options.fn(this);
+			  }
+			  return options.inverse(this);
+		}
+	}
+}));
 app.set("view engine", "hbs");
 app.set("views", [__dirname + "/views", __dirname + "/components"]);
 
