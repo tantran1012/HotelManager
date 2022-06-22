40e0f4bd129f8034837d4b4da5d2ca392f1a6ed3
tantran1012 <57386469+tantran1012@users.noreply.github.com>
2022-06-23 00:11:11 +0700

    Revert "Merge pull request #5 from tantran1012/ulrich"
    This reverts commit 99eb9b2a45eebef9f9e9acbf19f536359e061390, reversing
    changes made to e9c76f4b34bbf847a751278fb3acab3ca0d5a892.

--- components/bills/bills.router.js
+++ components/bills/bills.router.js
@@ -3,9 +3,6 @@ const router = express.Router();
 
 const billController = require('./bills.controller')
 
-router.put('/:id', billController.update);
-router.delete('/:id', billController.delete)
-router.post('/save', billController.save);
 router.get('/', billController.index);
 
 module.exports = router;
